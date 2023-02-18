eb1d54940b5afcd9c9ae17fc34b82bb0f0016835
matthewmeeks <matthewLmeeks@gmail.com>
2023-02-18 11:22:31 -0500

    license

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -48,12 +48,15 @@ invariant(
   `Could not find "build.options.outputPath" of project "${name}". Is project.json configured  correctly?`
 );
 
+const rootPackage = JSON.parse(readFileSync('package.json', 'utf8'));
+
 process.chdir(outputPath);
 
 // Updating the version in "package.json" before publishing
 try {
   const json = JSON.parse(readFileSync(`package.json`).toString());
   json.version = version;
+  json.license = rootPackage.license;
   writeFileSync(`package.json`, JSON.stringify(json, null, 2));
 } catch (e) {
   console.error(
